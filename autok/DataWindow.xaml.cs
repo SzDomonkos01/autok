cb32b2aec1c1a30caa68dba25663d601304b647e
kecske5000 <marton.kristof@students.jedlik.eu>
2025-04-28 16:15:57 +0200

    Inputcheck

---
+++ autok/DataWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace autok
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+
+        public Jarmu Jarmu { get; set; }
+
+        public DataWindow(Jarmu jarmu)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.Jarmu = jarmu;
+        }
+
+        private bool InputCheck()
+        {
+            if (String.IsNullOrWhiteSpace(Jarmu.rendszam))
+            {
+                MessageBox.Show("Az autó rendszámának megadása kötelező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(Jarmu.marka))
+            {
+                MessageBox.Show("Az autó márkájának megadása kötelező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Jarmu.forgalmi.HasValue)
+            {
+                MessageBox.Show("Az autó forgalmi engedélyének kiadási dátumát kötelező megadni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Jarmu.biztositas.HasValue)
+            {
+                MessageBox.Show("Az autó biztosításának kiadási dátumát kötelező megadni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Jarmu.evjarat.HasValue)
+            {
+                MessageBox.Show("Az autó gyártási évét kötelező megadni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(Jarmu.motorszam))
+            {
+                MessageBox.Show("Az autó motorszámának megadása kötelező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(Jarmu.alvazszam))
+            {
+                MessageBox.Show("Az autó alvázszámának megadása kötelező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (!Jarmu.forgalombah.HasValue)
+            {
+                MessageBox.Show("Az autó forgalomba helyezésének dátumát kötelező megadni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(Jarmu.model))
+            {
+                MessageBox.Show("Az autó modellének megadása kötelező!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- autok/MainWindow.xaml.cs
+++ autok/MainWindow.xaml.cs
@@ -14,63 +14,80 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace autok
+namespace autok;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public ObservableCollection<Jarmu>? Jarmuvek { get; set; }
+
+    public ObservableCollection<string> JarmuModels { get; set; }
+
+    public SearchFilters Filters { get; set; } = new SearchFilters();
+
+    public Jarmu SelectedJarmu { get; set; }
+
+    private ObservableCollection<Jarmu>? jarmuvekToShow;
+    public ObservableCollection<Jarmu>? JarmuvekToShow
     {
+        get { return jarmuvekToShow; }
+        set { jarmuvekToShow = value; OnPropertyChanged(nameof(JarmuvekToShow)); }
+    }
 
-        public SearchFilters Filters { get; set; } = new SearchFilters();
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged(string tulajdonsagNev)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tulajdonsagNev));
+    }
 
-        public Jarmu SelectedJarmu { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged(string tulajdonsagNev)
+    public MainWindow()
+    {
+        InitializeComponent();
+        this.DataContext = this;
+        FileRead();
+        JarmuModels = new(Jarmuvek.Select(x => x.marka).Distinct().Order());
+        JarmuModels.Insert(0, "");
+    }
+
+    private void FileRead()
+    {
+        string jsonStr = File.ReadAllText("jarmuvek.json");
+        Jarmuvek = JsonSerializer.Deserialize<ObservableCollection<Jarmu>>(jsonStr);
+        JarmuvekToShow = JsonSerializer.Deserialize<ObservableCollection<Jarmu>>(jsonStr);
+    }
+
+    private void search_BTN_Click(object sender, RoutedEventArgs e)
+    {
+        DateOnly ma = DateOnly.FromDateTime(DateTime.Today);
+
+        JarmuvekToShow = new(Jarmuvek.Where(jar =>
+            (string.IsNullOrEmpty(Filters.SelectedMarka) || jar.marka == Filters.SelectedMarka) &&
+            (!jar.evjarat.HasValue || jar.evjarat >= (int)Filters.SelectedEvjarat) &&
+            (!Filters.ForgalmiLejart || (jar.forgalmi.HasValue && jar.forgalmi.Value < ma)) &&
+            (!Filters.BiztositasLejart || (jar.biztositas.HasValue && jar.biztositas.Value < ma)) &&
+            (string.IsNullOrEmpty(Filters.RendszamSearch) || (jar.rendszam != null && jar.rendszam.Contains(Filters.RendszamSearch, StringComparison.OrdinalIgnoreCase)))
+        ));
+    }
+
+    private void JarmuAdd(string jarmuModelStr)
+    {
+        if (!JarmuModels.Contains(jarmuModelStr))
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(tulajdonsagNev));
+            JarmuModels.Add(jarmuModelStr);
         }
-
-        public ObservableCollection<Jarmu>? Jarmuvek { get; set; }
-
-        public ObservableCollection<string> JarmuModels { get; set; }
-
-        private ObservableCollection<Jarmu>? jarmuvekToShow;
-        public ObservableCollection<Jarmu>? JarmuvekToShow
+    }
+    private void new_BTN_Click(object sender, RoutedEventArgs e)
+    {
+        DataWindow dataWindow = new DataWindow(new Jarmu() { marka = Jarmuvek.Max(x => x.marka) + 1 });
+        dataWindow.ShowDialog();
+        if(dataWindow.DialogResult == true)
         {
-            get { return jarmuvekToShow; }
-            set { jarmuvekToShow = value; OnPropertyChanged(nameof(JarmuvekToShow)); }
-        }
-
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            this.DataContext = this;
-            FileRead();
-            JarmuModels = new(Jarmuvek.Select(x => x.marka).Distinct());
-            JarmuModels.Insert(0, "");
-        }
-
-        private void FileRead()
-        {
-            string jsonStr = File.ReadAllText("jarmuvek.json");
-            Jarmuvek = JsonSerializer.Deserialize<ObservableCollection<Jarmu>>(jsonStr);
-            JarmuvekToShow = JsonSerializer.Deserialize<ObservableCollection<Jarmu>>(jsonStr);
-        }
-
-        private void search_BTN_Click(object sender, RoutedEventArgs e)
-        {
-            DateOnly ma = DateOnly.FromDateTime(DateTime.Today);
-
-            JarmuvekToShow = new(Jarmuvek.Where(jar =>
-                (string.IsNullOrEmpty(Filters.SelectedMarka) || jar.marka == Filters.SelectedMarka) &&
-                (!jar.evjarat.HasValue || jar.evjarat >= (int)Filters.SelectedEvjarat) &&
-                (!Filters.ForgalmiLejart || (jar.forgalmi.HasValue && jar.forgalmi.Value < ma)) &&
-                (!Filters.BiztositasLejart || (jar.biztositas.HasValue && jar.biztositas.Value < ma)) &&
-                (string.IsNullOrEmpty(Filters.RendszamSearch) || (jar.rendszam != null && jar.rendszam.Contains(Filters.RendszamSearch, StringComparison.OrdinalIgnoreCase)))
-            ));
+            Jarmuvek.Add(dataWindow.Jarmu);
+            JarmuAdd(dataWindow.Jarmu.marka);
+            search_BTN_Click(sender, e);
         }
     }
 }
